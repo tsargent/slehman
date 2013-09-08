053d91dec10df2a2f70f350812a63dda8bbaf91c
Tyler Sargent <tsargent@gmail.com>
2013-09-08 13:46:48 -0400

    working on home image

--- app/assets/javascripts/site/home-image.js
+++ app/assets/javascripts/site/home-image.js
@@ -1,20 +1,26 @@
-function preloadImages(images, callback) {
-    var count = images.length;
-    if(count === 0) {
-        callback();
-    }
-    var loaded = 0;
-    $(images).each(function() {
-        $('<img>').attr('src', this).load(function() {
-            loaded++;
-            if (loaded === count) {
-                callback();
-            }
-        });
-    });
-};
+var imagesArray = ['/assets/steve_large.jpg'];
 
-// use whatever callback you really want as the argument
-preloadImages(["/assets/steve_large.jpg"], function() {
-    alert("DONE");
+$(function(){
+    function preloadImages(images, callback) {
+        var count = images.length;
+        if(count === 0) {
+            callback();
+        }
+        var loaded = 0;
+        $(images).each(function() {
+            $('<img>').attr('src', this).load(function() {
+                loaded++;
+                if (loaded === count) {
+                    callback();
+                }
+            });
+        });
+    };
+
+    var onImageLoad = function() {
+        var home_image = $('#home-image').attr('src', imagesArray[0]).hide();
+        home_image.fadeIn();
+    }
+
+    preloadImages(imagesArray, onImageLoad);
 });
