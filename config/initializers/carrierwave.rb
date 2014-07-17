2acd51e280abfbe6717965de2634f277ccf22475
Tyler Sargent <tsargent@vsapartners.com>
2014-07-17 09:25:11 -0400

    remove AWS info

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,11 +7,3 @@ CarrierWave.configure do |config|
   config.fog_directory = ENV['AWS_BUCKET']
   config.fog_public = true
 end
-
-
-# storage = Fog::Storage.new(:provider => 'AWS', :aws_access_key_id => 'AKIAIZ4MWNV6DLN4JA5A', :aws_secret_access_key => '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')
-
-
-
-# aws_access_key_id: 'AKIAIZ4MWNV6DLN4JA5A',
-# aws_secret_access_key: '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'
