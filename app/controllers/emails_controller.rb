404244ca0fc0099cc4367deb12943ab953c14e78
Tyler Sargent <tsargent@gmail.com>
2017-01-09 10:10:40 -0500

    use email blacklist

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -2,15 +2,18 @@ class EmailsController < ApplicationController
 
   def subscribe
     render :nothing => true
-    Gibbon::API.throws_exceptions = false    
+    Gibbon::API.throws_exceptions = false
     gb = Gibbon::API.new
     list = gb.lists.list({:filters => {:list_name => 'Steve Lehman'}})
     id = list['data'][0]['id']
     gb.lists.subscribe({:id => id, :email => {:email => params[:email]} })
   end
-  
+
   def message
-    UserMailer.message_email(params[:email], params[:message]).deliver
+    blacklist = ['jfvynms4281rt@hotmail.com']
+    email = params[:email]
+    message = params[:message]
+    UserMailer.message_email(email, message).deliver unless blacklist.include? email
     redirect_to contact_path, notice: 'Thanks for your message!'
   end
 
